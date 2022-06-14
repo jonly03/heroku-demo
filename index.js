e27b79128504b4e03ceb14f5b8738b315ccc5086
Nelly Sugu <jonly03@gmail.com>
2022-06-14 13:47:43 -0400

    added GET / endpoint

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+
+const PORT = 8080;
+
+server.listen(PORT, () => {
+  console.log("Server listening on PORT: ", PORT);
+});
+
+server.get("/", (req, res) => {
+  res.send({ day: "tueday", date: "06/14/22" });
+});
